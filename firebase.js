2f83641187853f02a46e7715f3e26af34fd05678
Amory Griffith <nguyenchingo28@gmail.com>
2022-08-18 14:32:15 +0700

    add firebase to the app

--- firebase.js
+++ firebase.js
@@ -1,5 +1,24 @@
-import {initializeApp} from 'firebase/app';
+// import firebase from "firebase";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBVgVCNoymmJ650hFKxJH2akqXNirP5aV0",
+//   authDomain: "restaurant-native-project.firebaseapp.com",
+//   projectId: "restaurant-native-project",
+//   storageBucket: "restaurant-native-project.appspot.com",
+//   messagingSenderId: "908819311286",
+//   appId: "1:908819311286:web:b1243bbfba8152ab6a9417"
+// };
+
+// !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+// export default firebase;
+
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyBVgVCNoymmJ650hFKxJH2akqXNirP5aV0',
   authDomain: 'restaurant-native-project.firebaseapp.com',
@@ -9,7 +28,5 @@ const firebaseConfig = {
   appId: '1:908819311286:web:b1243bbfba8152ab6a9417',
 };
 
-!firebaseConfig.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
-export default firebase;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
